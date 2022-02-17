e174e3c0b2eb8a2673500be2f9c4fe839216d594
Andrey Antukh <niwi@niwi.nz>
2022-02-17 22:45:21 +0100

    WIP

--- backend/src/app/http/debug/export.clj
+++ backend/src/app/http/debug/export.clj
@@ -25,6 +25,7 @@
    [cuerdas.core :as str]
    [datoteka.core :as fs]
    [ring.core.protocols :as rp]
+   [app.common.transit :as t]
    [fipp.edn :as fpp]
    [yetti.adapter :as yt])
   (:import
@@ -38,7 +39,20 @@
    com.github.luben.zstd.ZstdOutputStream
    org.apache.commons.io.input.BoundedInputStream))
 
+(set! *warn-on-reflection* true)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HELPERS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def ^:const buffer-size (:http/output-buffer-size yt/base-defaults))
+
+(def ^:const marks
+  {:header  1
+   :blob    2
+   :stream  3
+   :uuid    4
+   :transit 5})
 
 (defn buffered-output-stream
   "Returns a buffered output stream that ignores flush calls. This is
@@ -52,68 +66,226 @@
       (proxy-super flush)
       (proxy-super close))))
 
-(defn retrieve-file-media
-  [pool ids]
-  (with-open [conn (db/open pool)]
-    (let [sql "select * from file_media_object where id = ANY(?)"]
-      (db/exec! conn [sql (db/create-array conn "uuid" ids)]))))
+(defmacro assert-mark
+  [v type]
+  `(when (not= (long ~v) (~type marks))
+     (ex/raise :type :assertion
+               :code :unexpected-mark)))
+
+(defmacro assert-label
+  [n label]
+  `(when (not= ~n ~label)
+     (ex/raise :type :assertion
+               :code :unexpected-label)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; LOW LEVEL STREAM IO
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; --- PRIMITIVE
+
+(defn write-byte!
+  [^DataOutputStream ostream data]
+  (doto ostream
+    (.writeByte (int data))))
+
+(defn read-byte!
+  [^DataInputStream istream]
+  (.readByte istream))
+
+(defn write-long!
+  [^DataOutputStream ostream data]
+  (doto ostream
+    (.writeLong (long data))))
+
+(defn read-long!
+  [^DataInputStream istream]
+  (.readLong istream))
+
+(defn write-bytes!
+  [^DataOutputStream ostream ^bytes data]
+  (doto ostream
+    (.write data 0 (alength data))))
+
+(defn read-bytes!
+  [^DataOutputStream istream ^bytes buff]
+  (.read istream buff 0 (alength buff)))
+
+;; --- COMPOSITE
+
+(defn write-uuid!
+  [^DataOutputStream ostream id]
+  (doto ostream
+    (write-byte! (:uuid marks))
+    (write-long! (uuid/get-word-high id))
+    (write-long! (uuid/get-word-low id))))
+
+(defn read-uuid!
+  [^DataInputStream istream]
+  (let [m (read-byte! istream)]
+    (assert-mark m :uuid)
+    (let [a (read-long! istream)
+          b (read-long! istream)]
+      (uuid/custom b a))))
+
+(defn write-label!
+  [^DataOutputStream ostream label]
+  (let [^String label (if (keyword? label) (name label) label)
+        ^bytes data   (.getBytes label "UTF-8")]
+    (doto ostream
+      (write-long! (alength data))
+      (write-bytes! data))))
+
+(defn read-label!
+  [^DataInputStream istream]
+  (let [m (read-byte! istream)]
+    (assert-mark m :label)
+    (let [size (read-long! istream)
+          buff (byte-array size)]
+      (read-bytes! istream buff)
+      (keyword (String. buff "UTF-8")))))
+
+(defn write-header!
+  [^DataOutputStream ostream ^long version]
+  (doto ostream
+    (write-byte! (:header marks))
+    (write-label! :penpot-binary-export)
+    (write-long! version)))
+
+(defn read-header!
+  [^DataInputStream istream]
+  (let [m (read-byte! istream)]
+    (assert-mark m :header)
+    {:type (read-label! istream)
+     :version (read-long! istream)}))
+
+(defn write-blob!
+  [^DataOutputStream ostream label data]
+  (let [^bytes data (if (bytes? data) data (blob/encode data))]
+    (doto ostream
+      (write-byte! (:blob marks))
+      (write-label! label)
+      (write-long! (alength data))
+      (write-bytes! data))))
+
+(defn read-blob!
+  ([istream label] (read-blob! istream label false))
+  ([^DataInputStream istream label decode?]
+   (let [m (read-byte! istream)]
+     (assert-mark m :blob))
+   (let [n (read-label! istream)]
+     (assert-label n label))
+   (let [size (read-long! istream)
+         buff (byte-array size)]
+     (read-bytes! istream buff)
+     (if decode?
+       (blob/decode buff)
+       buff))))
+
+(defn copy-stream!
+  [^DataOutputStream ostream ^InputStream stream ^long size]
+  (let [buff (byte-array buffer-size)]
+    (IOUtils/copyLarge stream ostream 0 size buff)
+    stream))
+
+(defn write-stream!
+  [^DataOutputStream ostream label stream size]
+  (doto ostream
+    (write-byte! (:stream marks))
+    (write-label! label)
+    (write-long! size)
+    (copy-stream! stream size)))
+
+(defn read-stream!
+  [^DataInputStream istream label]
+  (let [m (read-byte! istream)]
+    (assert-mark m :blob))
+  (let [n (read-label! istream)]
+     (assert-label n label))
+  (let [size (read-long! istream)]
+    (doto (BoundedInputStream. istream size)
+      (.setPropagateClose false))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HIGH LEVEL IMPL
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(def storage-object-id-xf
+  (comp
+   (mapcat (juxt :media-id :thumbnail-id))
+   (filter uuid?)))
+
+(defn get-used-media
+  [pool fdata]
+  (let [fmedia-ids (#'tfm/collect-used-media fdata)]
+    (with-open [conn (db/open pool)]
+      (let [sql "select * from file_media_object where id = ANY(?)"]
+        (db/exec! conn [sql (db/create-array conn "uuid" ids)])))))
 
 (defn write-export!
   [{:keys [pool storage file-id]} ^DataOutputStream ostream]
-  (let [data    (some-> (db/get-by-id pool :file file-id) :data)
-        data'   (blob/decode data)
-        storage (media/configure-assets-storage storage)]
+  (let [{:keys [data] :as file} (db/get-by-id pool :file file-id)
 
-    ;; Write magic numbers
-    (.writeUTF ostream "PENPOT_CUSTOM_FILE")
+        fdata   (blob/decode data)
+        storage (media/configure-assets-storage storage)
+        fmedia  (get-used-media pool fdata)
+        sids    (into [] storage-object-id-xf fmedia)]
 
-    ;; Write data blob
-    (.writeLong ostream (alength data))
-    (.write ostream data 0 (alength data))
+    (doto ostream
+      (write-header! 1)
+      (write-blob! :file (dissoc file :data))
+      (write-blob! :fdata data)
+      (write-blob! :fmedia fmedia)
+      (write-blob! :sids sids))
 
-    (let [used  (#'tfm/collect-used-media data')
-          items (retrieve-file-media pool used)]
-      (.writeLong ostream (count items))
-      (doseq [{:keys [media-id thumbnail-id]} items]
-        (when-let [obj (some->> media-id (sto/get-object storage))]
-          (doto ostream
-            (.writeLong (uuid/get-word-high media-id))
-            (.writeLong (uuid/get-word-low media-id)))
-          (.writeLong ostream (:size obj))
-          (let [buff (byte-array buffer-size)]
-            (with-open [istream (sto/get-object-data storage obj)]
-              (IOUtils/copyLarge istream ostream 0 (:size obj) buff))))))))
+    (doseq [id sids]
+      (let [{:keys [size] :as obj} (sto/get-object storage id)]
+        (doto ostream
+          (write-uuid! id)
+          (write-blob! :storage-object obj)
+          (write-blob! :storage-metadata (meta obj)))
+
+        (with-open [stream (sto/get-object-data storage obj)]
+          (write-stream! ostream :storage-object-data stream size))))))
 
 (defn read-import!
   [{:keys [pool storage profile-id]} ^DataInputStream istream]
-  (let [mnum       (.readUTF istream)
-        file-id    (uuid/next)
-        project-id (some-> (profile/retrieve-additional-data pool profile-id) :default-project-id)]
+  (let [file-id    (uuid/next)
+        project-id (some-> (profile/retrieve-additional-data pool profile-id) :default-project-id)
+        header     (read-header! istream)]
 
     (when-not project-id
       (ex/raise :type :validation
                 :code :unable-to-lookup-project))
 
-    (when (not= mnum "PENPOT_CUSTOM_FILE")
+    (when (not= (:label header) :penpot-binary-export)
       (ex/raise :type :validation
                 :code :invalid-import-file))
 
-    (db/with-atomic [conn pool]
-      (let [blen    (.readLong istream)
-            bdata   (byte-array blen)
-            fname   (str "imported-custom-file-" (dt/now))
-            storage (media/configure-assets-storage storage conn)]
+    (when (not= (:version header) 1)
+      (ex/raise :type :validation
+                :code :unsupported-version))
 
-        (.readFully istream bdata 0 blen)
-        (m.files/create-file conn {:id file-id
-                                   :name fname
-                                   :project-id project-id
-                                   :profile-id profile-id
-                                   :data bdata})
+    (let [file   (read-blob! istream :file true)
+          fdata  (read-blob! istream :fdata false)
+          fmedia (read-blob! istream :fmedia true)
+          sids   (read-blob! istream :sids true)]
 
-        (let [nmedia (.readLong istream)]
-          (dotimes [i nmedia]
-            (prn "importing media" i)
+      (db/with-atomic [conn pool]
+        (let [storage (media/configure-assets-storage storage conn)]
+          (m.files/create-file conn {:id file-id
+                                     :name (:name file-id)
+                                     :project-id project-id
+                                     :profile-id profile-id
+                                     :data fdata})
+
+          (doseq [id sids]
+            (prn "importing media" id)
+            (let [stream (read-stream! istream :storage-object-data)
+                  content (sto/content stream size)]
+
+
             (let [huid    (.readLong istream)
                   luid    (.readLong istream)
                   size    (.readLong istream)
