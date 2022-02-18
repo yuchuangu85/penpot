1563c44327395dcddaa320ffa90dfa83fea0662c
Andrey Antukh <niwi@niwi.nz>
2022-02-18 14:02:13 +0100

    WIP

--- backend/src/app/http/debug/export.clj
+++ backend/src/app/http/debug/export.clj
@@ -15,8 +15,10 @@
    [app.media :as media]
    [app.config :as cf]
    [app.db :as db]
+   [app.common.pages.migrations :as pmg]
    [app.util.blob :as blob]
    [app.tasks.file-media-gc :as tfm]
+   [clojure.walk :as walk]
    [app.rpc.mutations.files :as m.files]
    [app.rpc.queries.profile :as profile]
    [app.util.time :as dt]
@@ -47,36 +49,46 @@
 
 (def ^:const buffer-size (:http/output-buffer-size yt/base-defaults))
 
-(def ^:const marks
-  {:header  1
-   :blob    2
-   :stream  3
-   :uuid    4
-   :transit 5})
+(defn get-mark
+  [id]
+  (case id
+    :header  1
+    :blob    2
+    :stream  3
+    :uuid    4
+    :label   5
+    (ex/raise :type :assertion
+              :code :invalid-mark-id
+              :hint (format "invalid mark id %s" id))))
 
-(defn buffered-output-stream
-  "Returns a buffered output stream that ignores flush calls. This is
-  needed because transit-java calls flush very aggresivelly on each
-  object write."
-  [^java.io.OutputStream os ^long chunk-size]
-  (proxy [java.io.BufferedOutputStream] [os (int chunk-size)]
-    ;; Explicitly do not forward flush
-    (flush [])
-    (close []
-      (proxy-super flush)
-      (proxy-super close))))
 
-(defmacro assert-mark
+;; (defn buffered-output-stream
+;;   "Returns a buffered output stream that ignores flush calls. This is
+;;   needed because transit-java calls flush very aggresivelly on each
+;;   object write."
+;;   [^java.io.OutputStream os ^long chunk-size]
+;;   (proxy [java.io.BufferedOutputStream] [os (int chunk-size)]
+;;     ;; Explicitly do not forward flush
+;;     (flush [])
+;;     (close []
+;;       (proxy-super flush)
+;;       (proxy-super close))))
+
+(defn assert-mark
   [v type]
-  `(when (not= (long ~v) (~type marks))
-     (ex/raise :type :assertion
-               :code :unexpected-mark)))
+  (let [expected (get-mark type)
+        v        (long v)]
+    (when (not= v expected)
+      (ex/raise :type :assertion
+                :code :unexpected-mark
+                :hint (format "received mark %s, expected %s" v expected)))))
 
-(defmacro assert-label
+(defn assert-label
   [n label]
-  `(when (not= ~n ~label)
-     (ex/raise :type :assertion
-               :code :unexpected-label)))
+  (when (not= n label)
+    (ex/raise :type :assertion
+              :code :unexpected-label
+              :hint (format "received label %s, expected %s" n label))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LOW LEVEL STREAM IO
@@ -108,7 +120,7 @@
     (.write data 0 (alength data))))
 
 (defn read-bytes!
-  [^DataOutputStream istream ^bytes buff]
+  [^DataInputStream istream ^bytes buff]
   (.read istream buff 0 (alength buff)))
 
 ;; --- COMPOSITE
@@ -116,7 +128,7 @@
 (defn write-uuid!
   [^DataOutputStream ostream id]
   (doto ostream
-    (write-byte! (:uuid marks))
+    (write-byte! (get-mark :uuid))
     (write-long! (uuid/get-word-high id))
     (write-long! (uuid/get-word-low id))))
 
@@ -126,13 +138,14 @@
     (assert-mark m :uuid)
     (let [a (read-long! istream)
           b (read-long! istream)]
-      (uuid/custom b a))))
+      (uuid/custom a b))))
 
 (defn write-label!
   [^DataOutputStream ostream label]
   (let [^String label (if (keyword? label) (name label) label)
         ^bytes data   (.getBytes label "UTF-8")]
     (doto ostream
+      (write-byte! (get-mark :label))
       (write-long! (alength data))
       (write-bytes! data))))
 
@@ -148,7 +161,7 @@
 (defn write-header!
   [^DataOutputStream ostream ^long version]
   (doto ostream
-    (write-byte! (:header marks))
+    (write-byte! (get-mark :header))
     (write-label! :penpot-binary-export)
     (write-long! version)))
 
@@ -163,7 +176,7 @@
   [^DataOutputStream ostream label data]
   (let [^bytes data (if (bytes? data) data (blob/encode data))]
     (doto ostream
-      (write-byte! (:blob marks))
+      (write-byte! (get-mark :blob))
       (write-label! label)
       (write-long! (alength data))
       (write-bytes! data))))
@@ -191,7 +204,7 @@
 (defn write-stream!
   [^DataOutputStream ostream label stream size]
   (doto ostream
-    (write-byte! (:stream marks))
+    (write-byte! (get-mark :stream))
     (write-label! label)
     (write-long! size)
     (copy-stream! stream size)))
@@ -199,17 +212,16 @@
 (defn read-stream!
   [^DataInputStream istream label]
   (let [m (read-byte! istream)]
-    (assert-mark m :blob))
+    (assert-mark m :stream))
   (let [n (read-label! istream)]
      (assert-label n label))
   (let [size (read-long! istream)]
-    (doto (BoundedInputStream. istream size)
-      (.setPropagateClose false))))
+    [size (doto (BoundedInputStream. istream size)
+            (.setPropagateClose false))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HIGH LEVEL IMPL
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 (def storage-object-id-xf
   (comp
@@ -218,7 +230,7 @@
 
 (defn get-used-media
   [pool fdata]
-  (let [fmedia-ids (#'tfm/collect-used-media fdata)]
+  (let [ids (#'tfm/collect-used-media fdata)]
     (with-open [conn (db/open pool)]
       (let [sql "select * from file_media_object where id = ANY(?)"]
         (db/exec! conn [sql (db/create-array conn "uuid" ids)])))))
@@ -246,64 +258,110 @@
           (write-blob! :storage-object obj)
           (write-blob! :storage-metadata (meta obj)))
 
-        (with-open [stream (sto/get-object-data storage obj)]
+        (with-open [^InputStream stream (sto/get-object-data storage obj)]
           (write-stream! ostream :storage-object-data stream size))))))
 
 (defn read-import!
   [{:keys [pool storage profile-id]} ^DataInputStream istream]
-  (let [file-id    (uuid/next)
-        project-id (some-> (profile/retrieve-additional-data pool profile-id) :default-project-id)
-        header     (read-header! istream)]
+  (letfn [(import-storage-object! [storage result id]
+            (let [sid   (read-uuid! istream)
+                  sobj  (read-blob! istream :storage-object true)
+                  smeta (read-blob! istream :storage-metadata true)]
+              (when (not= sid id)
+                (ex/raise :type :assertion
+                          :code :storage-object-id-does-not-match
+                          :hint (format "received storage id %s, expected %s" sid id)))
+              (let [[size stream] (read-stream! istream :storage-object-data)
+                    content       (sto/content stream size)
+                    params        (-> smeta
+                                      (assoc :content content)
+                                      (assoc :touched-at (dt/now))
+                                      (assoc :reference "file-media-object"))]
+                (assoc result id (sto/put-object storage params)))))
 
-    (when-not project-id
-      (ex/raise :type :validation
-                :code :unable-to-lookup-project))
+          (process-form [index form]
+            (cond-> form
+              ;; Relink Image Shapes
+              (and (map? form)
+                   (map? (:metadata form))
+                   (= :image (:type form)))
+              (update-in [:metadata :id] #(get index % %))))
 
-    (when (not= (:label header) :penpot-binary-export)
-      (ex/raise :type :validation
-                :code :invalid-import-file))
+          ;; A function responsible to analyze all file data and
+          ;; replace the old :component-file reference with the new
+          ;; ones, using the provided file-index
+          (relink-shapes [data index]
+            (walk/postwalk (partial process-form index) data))
 
-    (when (not= (:version header) 1)
-      (ex/raise :type :validation
-                :code :unsupported-version))
+          ;; A function responsible of process the :media attr of file
+          ;; data and remap the old ids with the new ones.
+          (relink-media [media index]
+            (reduce-kv (fn [res k v]
+                         (let [id (get index k)]
+                           (if (uuid? id)
+                             (-> res
+                                 (assoc id (assoc v :id id))
+                                 (dissoc k))
+                             res)))
+                       media
+                       media))]
 
-    (let [file   (read-blob! istream :file true)
-          fdata  (read-blob! istream :fdata false)
-          fmedia (read-blob! istream :fmedia true)
-          sids   (read-blob! istream :sids true)]
+    (let [file-id    (uuid/next)
+          project-id (some-> (profile/retrieve-additional-data pool profile-id) :default-project-id)
+          header     (read-header! istream)]
+
+
+      (when-not project-id
+        (ex/raise :type :validation
+                  :code :unable-to-lookup-project))
+
+      (when (or (not= (:type header) :penpot-binary-export)
+                (not= (:version header) 1))
+        (ex/raise :type :validation
+                  :code :invalid-import-file))
 
       (db/with-atomic [conn pool]
-        (let [storage (media/configure-assets-storage storage conn)]
+        (let [storage (media/configure-assets-storage storage conn)
+              file    (read-blob! istream :file true)
+              fdata   (read-blob! istream :fdata true)
+              fmedia  (read-blob! istream :fmedia true)
+              sids    (read-blob! istream :sids true)
+              objects (reduce (partial import-storage-object! storage) {} sids)
+
+              ;; Generate index of old->new fmedia id
+              index   (reduce #(assoc %1 (:id %2) (uuid/next)) {} fmedia)
+
+              fdata   (-> fdata
+                          (assoc :id file-id)
+                          (pmg/migrate-data)
+                          (update :pages-index relink-shapes index)
+                          (update :components relink-shapes index)
+                          (update :media relink-media index))]
+
+
+          ;; Create an empty filie
           (m.files/create-file conn {:id file-id
-                                     :name (:name file-id)
+                                     :name (:name file)
                                      :project-id project-id
                                      :profile-id profile-id
-                                     :data fdata})
+                                     :data (blob/encode fdata)})
 
-          (doseq [id sids]
-            (prn "importing media" id)
-            (let [stream (read-stream! istream :storage-object-data)
-                  content (sto/content stream size)]
-
-
-            (let [huid    (.readLong istream)
-                  luid    (.readLong istream)
-                  size    (.readLong istream)
-                  id      (uuid/custom luid huid)
-                  content (sto/content (doto (BoundedInputStream. istream size)
-                                         (.setPropagateClose false))
-                                       size)
-                  sobj    (sto/put-object storage {:content content})]
-              (db/insert! conn :file-media-object
-                          {:id (uuid/next)
-                           :file-id file-id
-                           :is-local false
-                           :name "test"
-                           :media-id (:id sobj)
-                           :thumbnail-id nil
-                           :width 100
-                           :height 100
-                           :mtype "image/png"}))))))))
+          (prn "----------------- INDEX")
+          (fipp.edn/pprint index)
+          (doseq [{:keys [media-id thumbnail-id] :as item} fmedia]
+            (let [params {:id (uuid/next)
+                          :file-id file-id
+                          :is-local (:is-local item)
+                          :name (:name item)
+                          :media-id (get index media-id)
+                          :thumbnail-id (get index thumbnail-id)
+                          :width (:width item)
+                          :height (:height item)
+                          :mtype (:mtype item)}]
+              (prn "=========================================")
+              (fipp.edn/pprint item)
+              (fipp.edn/pprint params)
+              (db/insert! conn :file-media-object params))))))))
 
 (defn export-handler
   [cfg request]
